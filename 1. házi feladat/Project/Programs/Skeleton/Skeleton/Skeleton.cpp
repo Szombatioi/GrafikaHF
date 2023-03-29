98e963da79e87e70a551ba8bf7849f418ab9be97
Szombatioi <o.szombati@gmail.com>
2023-03-29 21:36:55 +0200

    hf1_20230329_2136

--- 1. házi feladat/Project/Programs/Skeleton/Skeleton/Skeleton.cpp
+++ 1. házi feladat/Project/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -37,42 +37,53 @@
 //TODO: vetített koordináták fv., azokat kell a vectorba tenni
 //TODO: vetítés z koordinátája?
 
-class Geometria {
-	/*
+
+struct Point {
+	vec3 point, heading;
+	Point(vec3 p, vec3 h) : point(p), heading(h){}
+};
+
+
+/*
 	1. Egy irányra merőleges irány állítása.
 	2. Adott pontból és sebesség vektorral induló pont helyének és sebesség vektorának számítása t idővel később.
 	3. Egy ponthoz képest egy másik pont irányának és távolságának meghatározása.
 	4. Egy ponthoz képest adott irányban és távolságra lévő pont előállítása.
 	5. Egy pontban egy vektor elforgatása adott szöggel.
-	6. Egy közelítő pont és sebességvektorhoz a geometria szabályait teljesítő, közeli pont és sebesség választása.
-	*/
-public:
-	//1.
-	static vec3 iranyraMeroleges(vec3 vector) {
-		vec3 temp(0, 0, -1);
-		return cross(vector, temp);
-	}
+	6. Egy közelítő pont és sebességvektorhoz a Geometria szabályait teljesítő, közeli pont és sebesség választása.
+*/
 
-	//2.
-	static vec3 pontHelye(vec3 point, vec3 vector, float delta_t) {
-		return vec3( point * cosh(delta_t) + normalize(vector) * sinh(delta_t) );
-	}
+//1.
+vec3 perpendicular(vec3 vector) {
+	vec3 temp(0, 0, -1);
+	return cross(vector, temp);
+}
 
-	//3. ? 
+//2.
+Point animatePoint(vec3 point, vec3 vector, float delta_t) {
+	vec3 p = vec3(point * cosh(delta_t) + normalize(vector) * sinh(delta_t));
+	vec3 v = vec3(point * sinh(delta_t) + normalize(vector) * cosh(delta_t));
+	return Point(p, v);
+}
 
-	//4. TODO jó ez?
-	static vec3 ponthozKepestPont(vec3 point, vec3 vector) {
-		return vec3(point.x + vector.x, point.y + vector.y, point.z + vector.z);
-	}
+//3. ?
 
-	//5. TODO jó ez?
-	static vec3 rotateVector(vec3 vector, float angle) {
-		return vec3(vector * cos(angle) + iranyraMeroleges(vector)*sin(angle));
-	}
 
-	//6. 
+//4. TODO: szemek, szájak rajzolásához 
+vec3 pointByDirAndDist(vec3 point, vec3 dir, float dist) {
+	return animatePoint(point, dir, dist).point;
+}
+//vec3 producePoint(vec3 point, vec3 vector) {
+//	return vec3(point.x + vector.x, point.y + vector.y, point.z + vector.z);
+//}
 
-};
+//5. 
+vec3 rotateVector(vec3 vector, float angle) {
+	return vec3(vector * cos(angle) + perpendicular(vector) * sin(angle));
+}
+
+//6. ?
+//-------------------------
 
 class Renderer : public GPUProgram {
 	
@@ -110,15 +121,12 @@ public:
 	std::vector<vec2> project(const std::vector<vec3> points) { //TODO
 		std::vector<vec2> projected;
 		for (auto& point : points) {
-			vec2 temp = vec2(point.x / (point.z + 1.0),point.y / (point.z + 1.0));
-			projected.push_back(temp);
-			printf("p0: %.2f, %.2f\n", temp.x, temp.y);
+			projected.push_back(vec2(point.x / (point.z + 1.0), point.y / (point.z + 1.0)));
 		}
-		puts("Done");
 		return projected;
 	}
 
-	vec2 project(const vec3 point) { //TODO
+	vec2 project(const vec3 point) {
 		vec2 res = vec2(point.x / (point.z + 1.0), point.y / (point.z + 1.0));
 		return res;
 	}
@@ -144,6 +152,10 @@ Renderer* renderer;
 const int nOfCirclePoints = 30;
 long t;
 
+//for tests
+//boolean validPoint(vec3 point) { return point.x* point.x + point.y* point.y - point.z* point.z == -1; }
+//boolean validVector(vec3 vector, vec3 point) { return point.x*vector.x + point.y * vector.y + point.z * vector.z == 0; }
+
 class Circle {
 	vec3 centerPoint;
 	std::vector<vec3> circlePoints;
@@ -154,7 +166,7 @@ public:
 			float phi = i * 2.0f * M_PI / nOfCirclePoints;
 			float x = centerPoint.x + cosf(phi) * radius;
 			float y = centerPoint.y + sinf(phi) * radius;
-			circlePoints.push_back(vec3(x, y, 0));
+			circlePoints.push_back(vec3(x, y, 0)); //TODO
 		}
 	}
 	void draw(vec3 colors) {
@@ -166,14 +178,11 @@ class Hami {
 protected:
 	std::vector<vec2> slimePoints;
 	vec3 position, direction;
-	//float r; //nem kell, a pontok vetítése megoldja
-	std::vector<vec2> projectedPoints;
 	Circle centerCircle, leftEye, rightEye, leftPupil, rightPupil, mouth;
 public:
 	Hami() {}
-	Hami(vec3 start, vec3 dir = vec3(0,0,0)) {
-		centerCircle = Circle(start, 0.2f);
-		direction = dir;
+	Hami(vec3 start, vec3 dir) : centerCircle(start, 0.2f), direction(dir) {
+		leftEye = Circle(pointByDirAndDist(), 0.1);
 	}
 
 	virtual void move() { 
@@ -182,7 +191,7 @@ public:
 	virtual void draw(vec3 color){
 		animateMouth();
 		centerCircle.draw(color);
-		renderer->DrawGPU(GL_POINTS, slimePoints, vec3(1, 1, 1));
+		//renderer->DrawGPU(GL_POINTS, slimePoints, vec3(1, 1, 1));
 		/*leftEye.draw(vec3(1,1,1));
 		rightEye.draw(vec3(1, 1, 1));
 		rightPupil.draw(vec3(1, 1, 1));
@@ -191,8 +200,7 @@ public:
 	}
 
 	void rotate(boolean right, float phi = 0.1f) {
-		direction = Geometria::rotateVector(direction, right ? phi : -phi);
-		printf("%.2f, %.2f, %.2f\n", direction.x, direction.y, direction.z);
+		direction = Geometry::rotateVector(direction, right ? phi : -phi);
 	}
 
 	void animateMouth() {
@@ -212,38 +220,41 @@ public:
 	ZoldHami() {}
 	ZoldHami(vec3 start, vec3 dir) : Hami(start, dir) {}
 	void move() {
-		Hami::move();
+		//Hami::move();
 	}
 };
 
 Circle center;
 PirosHami pirosHami;
 ZoldHami zoldHami;
-//p + v*t
-std::vector<vec2> line;
-vec2 p, v;
+
+//testing the projection with lines
+std::vector<vec3> line;
 
 void onInitialization() {
+	
+	//printf("(2,2,3) %s\t(2,-2,0) %s\n", validPoint(vec3(0,0,1)) ? "valid" : "invalid", validVector(vec3(1,1,0), vec3(0, 0, 1)) ? "valid" : "invalid");
+
 	renderer = new Renderer();
 	center = Circle();
-	pirosHami = PirosHami(vec3(2,2,3), vec3(-1,0,0));
-	zoldHami = ZoldHami(vec3(0,0.5f,0), vec3(0,0,1));
+	pirosHami = PirosHami(vec3(0, 0, 1), vec3());
+	zoldHami = ZoldHami(vec3(2, 2, 3), vec3(2, -2, 0));
 
-	p = vec2(0, 0);
+	/*p = vec2(0, 0);
 	v = vec2(1, 2);
 	for (float t = 0; t < 100; t += 0.01) {
 		line.push_back(vec2(p + normalize(v)*t));
-	}
+	}*/
 }
 
 void onDisplay() {
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	//center.draw(vec3(0.5f, 0.5f, 0.5f));
-	/*pirosHami.draw(vec3(1,0,0));
-	zoldHami.draw(vec3(0,1,0));*/
+	pirosHami.draw(vec3(1,0,0));
+	//zoldHami.draw(vec3(0,1,0));
 
-	renderer->DrawGPU(GL_POINTS, line, vec3(1,1,1));
+	//renderer->DrawGPU(GL_POINTS, line, vec3(1,1,1));
 
 	//zoldHami.move(); //TODO: lehet nem ide kéne tenni
 	glutSwapBuffers();
