2417f902496d8fbdd0055a6c9b8ef20252b00b0e
Szombatioi <o.szombati@gmail.com>
2023-04-28 19:06:28 +0200

    hf2_init_20230428

---
+++ 2. házi feladat/Project/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -0,0 +1,157 @@
+#include "framework.h"
+
+class ImmediateModeRenderer2D : public GPUProgram {
+	const char* const vertexSource = R"(
+		#version 330
+		precision highp float;
+		layout(location = 0) in vec2 vertexPosition;	// Attrib Array 0
+
+		void main() { gl_Position = vec4(vertexPosition.x, vertexPosition.y, 0, 1); }	
+	)";
+
+	const char* const fragmentSource = R"(
+		#version 330
+		precision highp float;
+		uniform vec3 color;
+		out vec4 fragmentColor;	
+
+		void main() { fragmentColor = vec4(color, 1); }
+	)";
+
+	unsigned int vao, vbo; // we have just a single vao and vbo for everything :-(
+
+	int Prev(std::vector<vec2> polygon, int i) { return i > 0 ? i - 1 : polygon.size() - 1; }
+	int Next(std::vector<vec2> polygon, int i) { return i < polygon.size() - 1 ? i + 1 : 0; }
+
+	bool intersect(vec2 p1, vec2 p2, vec2 q1, vec2 q2) {
+		return (dot(cross(p2 - p1, q1 - p1), cross(p2 - p1, q2 - p1)) < 0 &&
+			dot(cross(q2 - q1, p1 - q1), cross(q2 - q1, p2 - q1)) < 0);
+	}
+
+	bool isEar(const std::vector<vec2>& polygon, int ear) {
+		int d1 = Prev(polygon, ear), d2 = Next(polygon, ear);
+		vec2 diag1 = polygon[d1], diag2 = polygon[d2];
+		for (int e1 = 0; e1 < polygon.size(); e1++) { // test edges for intersection
+			int e2 = Next(polygon, e1);
+			vec2 edge1 = polygon[e1], edge2 = polygon[e2];
+			if (d1 == e1 || d2 == e1 || d1 == e2 || d2 == e2) continue;
+			if (intersect(diag1, diag2, edge1, edge2)) return false;
+		}
+		vec2 center = (diag1 + diag2) / 2.0f; // test middle point for being inside
+		vec2 infinity(2.0f, center.y);
+		int nIntersect = 0;
+		for (int e1 = 0; e1 < polygon.size(); e1++) {
+			int e2 = Next(polygon, e1);
+			vec2 edge1 = polygon[e1], edge2 = polygon[e2];
+			if (intersect(center, infinity, edge1, edge2)) nIntersect++;
+		}
+		return (nIntersect & 1 == 1);
+	}
+
+	void Triangulate(const std::vector<vec2>& polygon, std::vector<vec2>& triangles) {
+		if (polygon.size() == 3) {
+			triangles.insert(triangles.end(), polygon.begin(), polygon.begin() + 2);
+			return;
+		}
+
+		std::vector<vec2> newPolygon;
+		for (int i = 0; i < polygon.size(); i++) {
+			if (isEar(polygon, i)) {
+				triangles.push_back(polygon[Prev(polygon, i)]);
+				triangles.push_back(polygon[i]);
+				triangles.push_back(polygon[Next(polygon, i)]);
+				newPolygon.insert(newPolygon.end(), polygon.begin() + i + 1, polygon.end());
+				break;
+			}
+			else newPolygon.push_back(polygon[i]);
+		}
+		Triangulate(newPolygon, triangles); // recursive call for the rest
+	}
+
+	std::vector<vec2> Consolidate(const std::vector<vec2> polygon) {
+		const float pixelThreshold = 0.01f;
+		vec2 prev = polygon[0];
+		std::vector<vec2> consolidatedPolygon = { prev };
+		for (auto v : polygon) {
+			if (length(v - prev) > pixelThreshold) {
+				consolidatedPolygon.push_back(v);
+				prev = v;
+			}
+		}
+		if (consolidatedPolygon.size() > 3) {
+			if (length(consolidatedPolygon.back() - consolidatedPolygon.front()) < pixelThreshold) consolidatedPolygon.pop_back();
+		}
+		return consolidatedPolygon;
+	}
+
+public:
+	ImmediateModeRenderer2D() {
+		glViewport(0, 0, windowWidth, windowHeight);
+		glLineWidth(2.0f); glPointSize(10.0f);
+
+		create(vertexSource, fragmentSource, "outColor");
+		glGenVertexArrays(1, &vao); glBindVertexArray(vao);
+		glGenBuffers(1, &vbo); 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(0);  // attribute array 0
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL);
+	}
+
+	void DrawGPU(int type, std::vector<vec2> vertices, vec3 color) {
+		setUniform(color, "color");
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vec2), &vertices[0], GL_DYNAMIC_DRAW);
+		glDrawArrays(type, 0, vertices.size());
+	}
+
+	void DrawPolygon(std::vector<vec2> vertices, vec3 color) {
+		std::vector<vec2> triangles;
+		Triangulate(Consolidate(vertices), triangles);
+		DrawGPU(GL_TRIANGLES, triangles, color);
+	}
+
+	~ImmediateModeRenderer2D() {
+		glDeleteBuffers(1, &vbo);
+		glDeleteVertexArrays(1, &vao);
+	}
+};
+
+ImmediateModeRenderer2D* renderer; // vertex and fragment shaders
+const int nTesselatedVertices = 30; //30 pontos kör
+
+std::vector<vec2> circlePoints, userPoints;
+
+void onInitialization() {
+	renderer = new ImmediateModeRenderer2D();
+	for (int i = 0; i < nTesselatedVertices; i++) {
+		float phi = 2 * M_PI * i / nTesselatedVertices;
+		circlePoints.push_back(vec2(cosf(phi), sinf(phi)));
+		userPoints.push_back(vec2(cosf(phi), sinf(phi)));
+	}
+}
+
+void onDisplay() {
+	glClearColor(100, 200, 150, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	renderer->DrawGPU(GL_LINE_LOOP, userPoints, vec3(1.0f, 0.5f, 0.2f));
+	renderer->DrawGPU(GL_TRIANGLE_FAN, circlePoints, vec3(0.0f, 0.0f, 0.5f));
+	
+	glutSwapBuffers();
+}
+
+void onMouse(int button, int state, int pX, int pY) { //no new points? :(
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		float cx = pX, cy = pY;
+		userPoints.push_back(vec2(cx,cy));
+		printf("px: %d, py: %d\n", pX, pY);
+		glutPostRedisplay();
+	}
+	
+}
+
+// Key of ASCII code pressed
+void onKeyboard(unsigned char key, int pX, int pY) {}
+// Key of ASCII code released
+void onKeyboardUp(unsigned char key, int pX, int pY) {}
+// Move mouse with key pressed
+void onMouseMotion(int pX, int pY) { }
+// Idle event indicating that some time elapsed: do animation here
+void onIdle() { }
