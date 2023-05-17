90cbb71ce8b31d58ad9409628526cce20c97f082
Szombatioi <o.szombati@gmail.com>
2023-05-17 21:01:33 +0200

    hf2_done_20230517_2100

--- 2. házi feladat/Project/Programs/Skeleton/Skeleton/Skeleton.cpp
+++ 2. házi feladat/Project/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -1,20 +1,46 @@
+//=============================================================================================
+// Masodik hazi feladat: Lehallgatastervezo
+//
+// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
+// Tilos:
+// - mast "beincludolni", illetve mas konyvtarat hasznalni
+// - faljmuveleteket vegezni a printf-et kiveve
+// - Mashonnan atvett programresszleteket forrasmegjeloles nelkul felhasznalni es
+// - felesleges programsorokat a beadott programban hagyni!!!!!!! 
+// - felesleges kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
+// ---------------------------------------------------------------------------------------------
+// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
+// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
+// a hazibeado portal ad egy osszefoglalot.
+// ---------------------------------------------------------------------------------------------
+// A feladatmegoldasokban csak olyan OpenGL fuggvenyek hasznalhatok, amelyek az oran a feladatkiadasig elhangzottak 
+// A keretben nem szereplo GLUT fuggvenyek tiltottak.
+//
+// NYILATKOZAT
+// ---------------------------------------------------------------------------------------------
+// Nev    : Szombati Oliver Istvan
+// Neptun : P37PLU
+// ---------------------------------------------------------------------------------------------
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
+// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
+// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
+//=============================================================================================
 #include "framework.h"
-#include <algorithm>
 
-const float epsilon = 0.0001f;
-//TODO:
-//nyilatkozat
-//kommentek torlese
-//forr�smegjel�l�sek
-//Cone
-//delete unnecessary classes
-//secondIntersect
+const float epsilon = 0.0001f, delta = 0.01;
 
 float dist_3D(vec3 p1, vec3 p2) {
 	vec3 v = p2 - p1;
 	return sqrt(dot(v,v));
 }
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 struct Material {
 	vec3 ka, kd, ks;
 	float shininess;
@@ -22,6 +48,7 @@ struct Material {
 	Material(vec3 _kd, vec3 _ks, float _shininess, vec3 da) : diffuseAlbedo(da), ka(_kd* M_PI), kd(_kd), ks(_ks) { shininess = _shininess; }
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 struct Hit {
 	float t;
 	vec3 position, normal;
@@ -29,11 +56,13 @@ struct Hit {
 	Hit() { t = -1; material = nullptr; }
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 struct Ray {
 	vec3 start, dir;
 	Ray(vec3 _start, vec3 _dir) : start(_start), dir(_dir) {}
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 class Intersectable {
 protected:
 	Material* material;
@@ -41,6 +70,7 @@ public:
 	virtual Hit intersect(const Ray& ray) = 0;
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 class Camera {
 	vec3 eye, lookat, right, up;
 	float fov;
@@ -59,6 +89,7 @@ public:
 	}
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 struct DLight {
 	vec3 direction, Le; //ir�ny, intenzit�s
 	DLight(vec3 _direction, vec3 _Le) {
@@ -67,6 +98,7 @@ struct DLight {
 	}
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 struct PLight {
 	vec3 location;
 	vec3 power;
@@ -93,7 +125,7 @@ class Triangle : public Intersectable{
 	vec3 normal;
 	Material* material;
 public:
-	Triangle(std::vector<vec3> _points, Material* _material){ //4 points
+	Triangle(std::vector<vec3> _points, Material* _material){
 		A = _points[0];
 		B = _points[1];
 		C = _points[2];
@@ -106,6 +138,7 @@ public:
 		Triangle(p, _material);
 	}
 
+	//Forras: Az eloadasfoliak alapjan
 	Hit intersect(const Ray& ray) {
 		Hit hit;
 		float t = dot((A - ray.start), normal) / dot(ray.dir, normal);
@@ -136,7 +169,7 @@ struct Cone : public Intersectable {
 		vec3 start_to_point = ray.start - p;
 		float cos_square = pow(cosf(angle), 2);
 		float a = pow(dot(ray.dir, n), 2) - dot(ray.dir, ray.dir) * cos_square;
-		float b = 2.0f * (dot(ray.dir, n) * dot(start_to_point, n) - cos_square * dot(ray.dir, start_to_point)); //todo: lehet p-s kell ide hátulra
+		float b = 2.0f * (dot(ray.dir, n) * dot(start_to_point, n) - cos_square * dot(ray.dir, start_to_point));
 		float c = dot(n, start_to_point)* dot(n, start_to_point) - dot(start_to_point, start_to_point) * cos_square;
 		
 		float discr = b * b - 4.0f * a * c;
@@ -145,7 +178,6 @@ struct Cone : public Intersectable {
 		float t1 = (-b + sqrt_discr) / 2.0f / a;
 		float t2 = (-b - sqrt_discr) / 2.0f / a;
 		if (t1 <= 0 && t2 <= 0) return hit;
-		//hit.t = (t2 >= 0 && t2 <= t1) ? t2 : t1;
 
 		float d1 = dot(ray.start + ray.dir * t1 - p, n);
 		float d2 = dot(ray.start + ray.dir * t2 - p, n);
@@ -156,24 +188,13 @@ struct Cone : public Intersectable {
 			hit.t = t2;
 		}
 
-
-		/*hit.t = t1;
-		if (hit.t < 0.0 || t2 >= 0.0 && t2 <= hit.t) hit.t = t2;
-		if (hit.t < 0.0) return Hit();*/
-
-		/*hit.t = t1;
-		if (hit.t < 0 || t2>0. && t2 < hit.t) hit.t = t2;
-		if (hit.t < 0) return Hit();*/
-
-		vec3 cp = ray.start + hit.t * ray.dir - p;
-		float h = dot(cp, n);
+		vec3 intP = ray.start + hit.t * ray.dir - p;
+		float h = dot(intP, n);
 		if (h < 0.0 || h > height) return Hit();
 
-		
 		vec3 point = ray.start + ray.dir * hit.t;
 		hit.position = point;
 		hit.normal = normalize(2*dot((point - p),n)*n - 2*(point - p)*cos_square);
-		//hit.normal = normalize(cp * dot(n, cp) / dot(cp,cp) - n);
 		hit.material = material;
 		return hit;
 	}
@@ -182,31 +203,17 @@ struct Cone : public Intersectable {
 struct Bug {
 	Cone* cone;
 	PLight* light;
-//public:
 	Bug(Cone* c, PLight* l) : cone(c), light(l) {}
 	void Move(vec3 to, vec3 normal) {
 		cone->p = to;
 		cone->n = normalize(normal);
-
-		light->location = to + cone->n * 0.05;
+		light->location = to + cone->n * delta;
 	}
 };
 
 class Square : public Intersectable {
 	std::vector<Triangle> triangles; //2
 public:
-	/*Square(std::vector<vec3> points, std::vector<int> panes, Material* _material) {
-		
-	}*/
-	
-	//TODO: delete
-	/*Square(std::vector<Triangle> t) {
-		triangles = std::vector<Triangle>();
-		triangles.push_back(t[0]);
-		triangles.push_back(t[1]);
-
-	}*/
-
 	Square(vec3 A, vec3 B, vec3 C, vec3 D, Material* mat) {
 		triangles = std::vector<Triangle>();
 		std::vector<vec3> t1 = { A, B, C };
@@ -214,7 +221,6 @@ public:
 		triangles.push_back(Triangle(t1, mat));
 		triangles.push_back(Triangle(t2, mat));
 	}
-
 	Hit intersect(const Ray& ray) {
 		Hit bestHit;
 		for (Triangle t : triangles) {
@@ -236,10 +242,6 @@ public:
 		}
 	}
 
-	Cube(std::vector<Square> s) {
-		sides = s;
-	}
-
 	Hit intersect(const Ray& ray) {
 		Hit bestHit, secondBestHit;
 		for (Square s : sides) {
@@ -250,12 +252,9 @@ public:
 			}
 		}
 		return secondBestHit;
-		//return bestHit;
 	}
 };
 
-
-//TODO: Triangles Square helyett
 class Icosahedron : public Intersectable {
 	std::vector<Square> sides;
 public:
@@ -264,9 +263,6 @@ public:
 		for (int i = 0; i < panes.size(); i += 4) {
 			sides.push_back(Square(points[panes[i]], points[panes[i + 1]], points[panes[i + 2]], points[panes[i+3]], material));
 		}
-	}
-	Icosahedron(std::vector<Square> s) {
-		sides = s;
 	}
 	Hit intersect(const Ray& ray) {
 		Hit bestHit;
@@ -287,9 +283,6 @@ public:
 			sides.push_back(Square(points[panes[i]], points[panes[i + 1]], points[panes[i + 2]], points[panes[i + 3]], material));
 		}
 	}
-	Octahedron(std::vector<Square> s) {
-		sides = s;
-	}
 	Hit intersect(const Ray& ray) {
 		Hit bestHit;
 		for (Square s : sides) {
@@ -300,10 +293,10 @@ public:
 	}
 };
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 class Scene {
 	std::vector<Intersectable*> objects;
 	std::vector<DLight*> lights;
-	std::vector<PLight*> coneLights;
 	std::vector<Bug*> bugs;
 	Camera camera;
 	vec3 La;
@@ -318,7 +311,7 @@ public:
 		lights.push_back(new DLight(lightDirection, Le));
 
 		vec3 kd1(0.3f, 0.3f, 0.3f), ks(1,1,1);
-		Material* material = new Material(kd1, ks, 75, vec3(1,1,1)); //50 helyett 75
+		Material* material = new Material(kd1, ks, 75, vec3(1,1,1));
 
 		vec3 A, B, C, D, E, F, G, H, I, J, K, L;
 
@@ -334,7 +327,6 @@ public:
 		J = vec3(-0.25f, 0.075f , 0.235f),
 		K = vec3(-0.25f, 0.075f, 0.765f),
 		L = vec3(-0.25f, 0.925f, 0.765f);		
-
 		std::vector<vec3> icosaPoints = { A,B,C,D,E,F,G,H,I,J,K,L };
 		std::vector<int> icosaPointsIdx = {
 			6,1,2,7,
@@ -350,14 +342,13 @@ public:
 		};
 		objects.push_back(new Icosahedron(icosaPoints, icosaPointsIdx, material));
 		
-		//Octahedron - squares
+
 		A = vec3(1.0f, 0.5f, -0.2f),
 		B = vec3(0.5f, 0.0f, -0.2f),
 		C = vec3(0.0f, 0.5f, -0.2f),
 		D = vec3(0.5f, 1.0f, -0.2f),
 		E = vec3(0.5f, 0.5f, 0.3f),	
 		F = vec3(0.5f, 0.5f, -0.8f);
-
 		std::vector<vec3> octaPoints = {A,B,C,D,E,F};
 		std::vector<int> octaPointsIdx = {
 			0,1,4,2,
@@ -366,6 +357,7 @@ public:
 			2,3,5,0
 		};
 		objects.push_back(new Octahedron(octaPoints, octaPointsIdx, material));
+
 
 		A = vec3(-1.0, 0.0, -1.0),
 		B = vec3(-1.0, 2.0, -1.0),
@@ -377,27 +369,22 @@ public:
 		H = vec3(1.0, 2.0, 1.0);
 		std::vector<vec3> cubePoints = { A,B,C,D,E,F,G,H };
 		std::vector<int> cubePointIdx = {
-			4,6,0,2, //padló
-			2,3,0,1, //bal hátsó oldal
-			4,5,0,1, //jobb hátsó oldal
-			5,7,1,3, //tető
-			6,7,4,5, //jobb első oldal
-			6,7,2,3 //bal első oldal
+			4,6,0,2,
+			2,3,0,1,
+			4,5,0,1,
+			5,7,1,3,
+			6,7,4,5,
+			6,7,2,3
 		};
 		objects.push_back(new Cube(cubePoints, cubePointIdx, material));
 
 		Cone* c1 = new Cone(vec3(0, 2, 0), vec3(0, -1, 0), 0.2, M_PI / 8, material); //red
 		Cone* c2 = new Cone(vec3(-1, 1.5, 0), vec3(1,0,0), 0.2, M_PI / 8, material); //green
-		//Cone* c3 = new Cone(vec3(0.67, 0.67, -0.03), vec3(0.25, 0.25, 0.25), 0.2, M_PI / 8, material); //blue
 		Cone* c3 = new Cone(vec3(0.064, 0.735, 0.359), vec3(0.577, 0.577, -0.577), 0.2, M_PI / 8, material); //blue	
 
-		float delta = 0.01;
 		PLight* p1 = new PLight(c1->p + c1->n * delta, vec3(150,0,0));
 		PLight* p2 = new PLight(c2->p + c2->n * delta, vec3(0,150,0));
 		PLight* p3 = new PLight(c3->p + c3->n * delta, vec3(0,0,150));
-		coneLights.push_back(p1);
-		coneLights.push_back(p2);
-		coneLights.push_back(p3);
 
 		objects.push_back(c1);
 		objects.push_back(c2);
@@ -407,7 +394,8 @@ public:
 		bugs.push_back(new Bug(c3, p3));
 	}
 
-	void render(std::vector<vec4>& image) { //virt. vil�g lef�nyk�pez�se
+	//Forras: Szirmay-Kalos Laszlo kodjai alapjan
+	void render(std::vector<vec4>& image) {
 		for (int Y = 0; Y < windowHeight; Y++) {
 #pragma omp parallel for
 			for (int X = 0; X < windowWidth; X++) {
@@ -417,6 +405,7 @@ public:
 		}
 	}
 
+	//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 	Hit firstIntersect(Ray ray) {
 		Hit bestHit;
 		for (Intersectable* obj : objects) {
@@ -428,15 +417,11 @@ public:
 		if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
 		return bestHit;
 	}
-
-	bool shadowIntersect(Ray ray) {	// for directional lights
+	
+	//Forras: Szirmay-Kalos Laszlo kodjai alapjan
+	bool shadowIntersect(Ray ray) {
 		for (Intersectable* object : objects) if (object->intersect(ray).t > 0) return true;
 		return false;
-	}
-
-	Hit clickTrace(Ray ray) {
-		Hit hit = firstIntersect(ray);
-		return hit;
 	}
 
 
@@ -457,7 +442,6 @@ public:
 			}
 		}
 
-		//float delta = 0.0;
 		vec3 outRad(0, 0, 0);
 		vec3 N = hit.normal;
 		vec3 outDir;
@@ -465,7 +449,7 @@ public:
 			PLight *light = bug->light;
 			outDir = light->directionOf(hit.position + hit.normal * epsilon);
 			Hit shadowHit = firstIntersect(Ray(hit.position + N * epsilon, outDir));
-			if (shadowHit.t < 0. || shadowHit.t > light->distanceOf(hit.position + hit.normal * epsilon)) {
+			if (shadowHit.t < 0.0f || shadowHit.t > light->distanceOf(hit.position + hit.normal * epsilon)) {
 				double cosThetaL = dot(N, outDir);
 				outRad = outRad + hit.material->diffuseAlbedo / M_PI * cosThetaL * light->radianceAt(hit.position + hit.normal * epsilon);
 			}
@@ -475,24 +459,19 @@ public:
 	}
 
 	void mouseClick(int pX, int pY) {
-		//printf("%d, %d\n", pX, pY);
-
-
 		Ray ray = camera.getRay(pX, pY);
-		Hit hit = clickTrace(ray);
-		printf("%lf,%lf,%lf\n%lf,%lf,%lf\n", hit.position.x, hit.position.y, hit.position.z, hit.normal.x, hit.normal.y, hit.normal.z);
+		Hit hit = firstIntersect(ray);
 
 		Bug* closestBug;
 		float dist = INT_MAX;
 
-		for (Bug* b : bugs) { //ezzel kiválasztjuk a legközelebbi Cone-t
+		for (Bug* b : bugs) {
 			float d = dist_3D(b->cone->p, hit.position);
 			if (d < dist) {
 				dist = d;
 				closestBug = b;
 			}
 		}
-
 		closestBug->Move(hit.position, normalize(hit.normal));
 	}
 };
@@ -500,57 +479,59 @@ public:
 GPUProgram gpuProgram;
 Scene scene;
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 const char *vertexSource = R"(
 	#version 330
     precision highp float;
 
-	layout(location = 0) in vec2 cVertexPosition;	// Attrib Array 0
+	layout(location = 0) in vec2 cVertexPosition;
 	out vec2 texcoord;
 
 	void main() {
-		texcoord = (cVertexPosition + vec2(1, 1))/2;							// -1,1 to 0,1
-		gl_Position = vec4(cVertexPosition.x, cVertexPosition.y, 0, 1); 		// transform to clipping space
+		texcoord = (cVertexPosition + vec2(1, 1))/2;
+		gl_Position = vec4(cVertexPosition.x, cVertexPosition.y, 0, 1);
 	}
 )";
 
+//Forras: Szirmay-Kalos Laszlo kodjai alapjan
 const char *fragmentSource = R"(
 	#version 330
     precision highp float;
 
 	uniform sampler2D textureUnit;
-	in  vec2 texcoord;			// interpolated texture coordinates
-	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
+	in  vec2 texcoord;
+	out vec4 fragmentColor;
 
 	void main() {
 		fragmentColor = texture(textureUnit, texcoord); 
 	}
 )";
 
+//Forras: Szirmay-Kalos Laszlo kodjaibol
 class FullScreenTexturedQuad {
-	unsigned int vao;	// vertex array object id and texture id
+	unsigned int vao;
 	Texture texture;
 public:
 	FullScreenTexturedQuad(int windowWidth, int windowHeight, std::vector<vec4>& image)
 		: texture(windowWidth, windowHeight, image) 
 	{
-		glGenVertexArrays(1, &vao);	// create 1 vertex array object
-		glBindVertexArray(vao);		// make it active
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-		unsigned int vbo;		// vertex buffer objects
-		glGenBuffers(1, &vbo);	// Generate 1 vertex buffer objects
+		unsigned int vbo;
+		glGenBuffers(1, &vbo);
 
-		// vertex coordinates: vbo0 -> Attrib Array 0 -> vertexPosition of the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo); // make it active, it is an array
-		float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };	// two triangles forming a quad
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);     // stride and offset: it is tightly packed
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 	}
 
 	void Draw() {
-		glBindVertexArray(vao);	// make the vao and its vbos active playing the role of the data source
+		glBindVertexArray(vao);
 		gpuProgram.setUniform(texture, "textureUnit");
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);	// draw two triangles forming a quad
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 	}
 };
 
